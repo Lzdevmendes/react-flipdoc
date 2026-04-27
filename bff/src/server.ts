ad562d3903b0960e19d637c2e90484eba916e688
Lzdevmendes <lzmendestechdev@gmail.com>
2026-04-27 15:14:41 -0300

    Read CORS origin from CORS_ORIGIN environment variable

--- bff/src/server.ts
+++ bff/src/server.ts
@@ -8,7 +8,7 @@ const app = express()
 
 // CORS configuration
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: process.env.CORS_ORIGIN || 'http://localhost:5173',
   credentials: true
 }))
 
